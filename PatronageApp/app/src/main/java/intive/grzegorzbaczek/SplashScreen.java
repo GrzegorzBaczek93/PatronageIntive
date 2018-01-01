a52062dd144ad4ba7f76f51d2f7089cd0c18afab
Bumbl3Bee <grzegorz.baczek.93@gmail.com>
2018-01-01 16:18:15 +0100

    Finished splash screen and added basic menu activity

--- PatronageApp/app/src/main/java/intive/grzegorzbaczek/SplashScreen.java
+++ PatronageApp/app/src/main/java/intive/grzegorzbaczek/SplashScreen.java
@@ -1,13 +1,45 @@
 package intive.grzegorzbaczek;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static final int waitingTime = 5000;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        finishAndRemoveTask();
+        System.exit(0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ActivityLoader loader = new ActivityLoader();
+        loader.start();
+    }
+
+    private class ActivityLoader extends Thread {
+
+        @Override
+        public void run() {
+
+            try {
+                Thread.sleep(waitingTime);
+            } catch (InterruptedException e) {
+                Log.e("Splash screen", e.getMessage());
+            }
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
