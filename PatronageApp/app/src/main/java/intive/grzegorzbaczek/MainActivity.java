7798db7c1085518b1d76180e9488c7a351bf81f7
Bumbl3Bee <grzegorz.baczek.93@gmail.com>
2018-01-01 17:44:58 +0100

    Filled homepage with content

--- PatronageApp/app/src/main/java/intive/grzegorzbaczek/MainActivity.java
+++ PatronageApp/app/src/main/java/intive/grzegorzbaczek/MainActivity.java
@@ -1,8 +1,11 @@
 package intive.grzegorzbaczek;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private int lastUsedFragment = R.id.nav_homepage;
+    private String login = null;
+    private String authorizationData = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +31,8 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -38,8 +40,20 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        if (savedInstanceState != null) {
+            lastUsedFragment = savedInstanceState.getInt("_lastUsedFragment");
+            displayView(savedInstanceState.getInt("_lastUsedFragment"));
+        } else {
+            displayView(R.id.nav_homepage);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("_lastUsedFragment", lastUsedFragment);
+        outState.putString("_login", login);
+        outState.putString("_authorizationData", authorizationData);
     }
 
     @Override
@@ -52,50 +66,53 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    public void displayView(int viewId) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int title = 0;
+        Fragment fragment = null;
+        Class fragmentClass = null;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (viewId) {
+            case R.id.nav_settings:
+                fragmentClass = SettingsFragment.class;
+                title = R.string.settings;
+                break;
+            case R.id.nav_about:
+                fragmentClass = AboutFragment.class;
+                title = R.string.about;
+                break;
+            case R.id.nav_exit:
+                finishAndRemoveTask();
+                System.exit(0);
+                break;
+            default:
+                fragmentClass = HomepageFragment.class;
+                title = R.string.homepage;
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (fragmentClass != null) {
+            getSupportActionBar().setTitle(getString(title));
+
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.contentPanel, fragment).commit();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        lastUsedFragment = item.getItemId();
+        displayView(item.getItemId());
         return true;
     }
 }
